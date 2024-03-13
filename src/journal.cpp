e96462079f5b0e8d9ade2c5ef47a6ac0eaa510d5
Afsa Nafe <afsanafe1@gmail.com>
2024-03-13 15:35:26 -0400

    function stubs for journal class

---
+++ src/journal.cpp
@@ -0,0 +1,23 @@
+#include "journal.hpp"
+#include <string>
+
+
+std::string& Journal::getEntry(std::string& entryName) {
+    std::string res = "hi";
+    return res;
+}
+
+bool insertEntry(std::string& note) {
+
+    return true;
+}
+
+bool deleteEntry(std::string& entryName) {
+
+    return true;
+}
+
+bool modifyEntry(std::string& entryName) {
+
+    return true;
+}
